60ff0a3f5005d112baf2140ccf8beaed37ef1fd7
Enrique Gan <PitEG@github.com>
2022-05-28 06:02:04 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,18 @@ fn main() {
         },
     };
     
-    let songs = songdb::get_meta_dir("/home/enrique/nextcloud/music");
+    /*
+    let songs = songdb::get_meta_dir("/home/enrique/Music/big");
+    println!("{}", songs.len());
     for song in songs {
-        db.add(&song);
+        let _ = db.add(&song);
     }
+    */
+
+    match db.prune_db() {
+        Ok(_) => (),
+        Err(e) => println!("{}",e),
+    }; 
 
     // let player = player::create(db);
     // let _ = player.start();
