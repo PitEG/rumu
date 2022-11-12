a8bac6175f61224a483b3ecc331414d81699ba85
Enrique Gan <enriqueganpit@gmail.com>
2022-11-11 21:45:44 -0800

    added up and down for navigator

--- src/app/navigator.rs
+++ src/app/navigator.rs
@@ -7,38 +7,28 @@ struct Category {
 
 pub struct Navigator {
     items: Vec<(Category,bool,Vec<String>)>,
-    selection: (u32,Option<i32>),
-    search_query: Option<String>,
+    selection: (i32,Option<i32>),
+    search_query: Option<(String,String)>,
 }
+
 
 impl Command for Navigator {
     fn command(&mut self, event: &Event) {
-        // move up and down the subcategory
-        self.selection.1 = match event {
-            Event::Up => self.selection.1.and_then(|v| Some(v-1)),
-            Event::Down => self.selection.1.and_then(|v| Some(v+1)),
-            _ => self.selection.1,
+        // 
+        // UP/DOWN: move selection
+        //
+        match event {
+            Event::Up => {self.back()},
+            Event::Down => {self.next()},
+            _ => {},
         };
-        // check if we leave subcategory and move if we do // SHADY
-        self.selection.1 = self.selection.1.and_then(|v| {
-            let subcategory_size = self.items[self.selection.0 as usize].2.len() as u32;
-            if v < 0 { 
-                self.selection.0 = std::cmp::min(subcategory_size-1,self.selection.0.overflowing_sub(1).0);
-                return None; 
-            }
-            else if v >= subcategory_size as i32 { 
-                self.selection.0 = (self.selection.0 + 1) % subcategory_size;
-                return None; 
-            }
-            return Some(v);
-        });
     }
 }
 
 impl Navigator {
-    pub fn get_search(&mut self) -> Option<String> {
+    pub fn get_search(&mut self) -> Option<(String,String)> {
         let search = match &self.search_query {
-            Some(x) => Some(x.clone()),
+            Some(x) => Some((x.0.clone(),x.1.clone())),
             None => None,
         };
         self.search_query = None;
@@ -46,6 +36,71 @@ impl Navigator {
     }
 
     pub fn get_selection(&self) -> (u32,Option<u32>) {
-        return self.selection;
+        return (self.selection.0 as u32, 
+                self.selection.1.and_then(|v| Some(v as u32)));
+    }
+
+    fn size_of_category(&self, pos : usize) -> i32 {
+        return self.items[pos].2.len() as i32;
+    }
+
+    fn next(&mut self) {
+        self.selection.1 = match self.selection.1 {
+            Some(v) => {
+                // subcat -> subcat
+                let mut result = Some(v + 1);
+                // subcat -> cat
+                if (v + 1) > self.size_of_category(self.selection.0 as usize) {
+                    self.selection.0 += 1;
+                    self.selection.0 %= self.items.len() as i32;
+                    result = None;
+                }
+                result
+            },
+            None => {
+                let mut result = None;
+                // cat -> subcat
+                if self.items[self.selection.0 as usize].1 { // if open
+                    result = Some(0);
+                }
+                // cat -> cat
+                else {
+                    self.selection.0 += 1;
+                    self.selection.0 %= self.items.len() as i32;
+                }
+                result
+            },
+        };
+    }
+
+    fn back(&mut self) {
+        self.selection.1 = match self.selection.1 {
+            Some(v) => {
+                // subcat -> subcat
+                let mut result = Some(v - 1);
+                // subcat -> cat
+                if (v - 1) < 0 {
+                    self.selection.0 -= 1;
+                    if self.selection.0 < 0 {
+                        self.selection.0 = self.items.len() as i32;
+                    }
+                    result = None
+                }
+                result
+            }
+            None => {
+                let mut result = None;
+                self.selection.0 -= 1;
+                if self.selection.0 < 0 {
+                    self.selection.0 = self.items.len() as i32;
+                }
+                // cat -> subcat
+                if self.items[self.selection.0 as usize].1 {
+                    result = Some(self.size_of_category(self.selection.0 as usize) - 1);
+                }
+                // cat -> cat // default when the above if block doesn't run
+                result
+            },
+        }
     }
 }
